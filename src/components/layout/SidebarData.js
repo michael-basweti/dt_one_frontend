a8682930d70cd1b9737c0bfb3fcb7e1f9e0c42fa
michael-basweti <baswetima@gmail.com>
2022-02-21 18:33:04 +0300

    remove sidebar

--- src/components/layout/SidebarData.js
+++ src/components/layout/SidebarData.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import {GiCarWheel,GiCartwheel,GiMechanicGarage} from 'react-icons/gi';
-// import {AiFillHome} from 'react-icons/ai';
-// import {IoIosPaper, IoIosPricetags} from 'react-icons/io';
-// import {ImUserPlus,ImUsers} from 'react-icons/im';
-import {MdCreateNewFolder,MdOutlineRequestQuote} from 'react-icons/md';
 import {RiDashboardLine} from 'react-icons/ri'
 import {RiArrowDownSFill,RiArrowUpSFill} from 'react-icons/ri';
 
@@ -18,56 +13,6 @@ export const admin_side = [
     iconClosed: <RiArrowDownSFill />,
     iconOpened: <RiArrowUpSFill />,
   },
-  {
-    title: 'Debt  Mngt',
-    path: '#',
-    icon: <MdCreateNewFolder />,
-    cName: 'nav-text',
-    iconClosed: <RiArrowDownSFill />,
-    iconOpened: <RiArrowUpSFill />,
-    subNav: [
-      {
-        title: 'Add Debt Collectors',
-        path: '/collectors',
-        icon: <GiCarWheel />
-      },
-      {
-        title: 'Assign Invoices',
-        path: '/invoice_assign',
-        icon: <GiCartwheel />
-      },
-      {
-        title: 'Assign Customers',
-        path: '/customer_assign',
-        icon: <GiCartwheel />
-      },
-      {
-        title: 'Invoice Status',
-        path: '/invoice_status',
-        icon: <GiCartwheel />
-      }
-    ]
-  },
-  {
-    title: 'User Mngt',
-    path: '#',
-    icon: <GiMechanicGarage />,
-    cName: 'nav-text',
-    iconClosed: <RiArrowDownSFill />,
-    iconOpened: <RiArrowUpSFill />,
-    subNav: [
-      {
-        title: 'Add Users',
-        path: '/users',
-        icon: <GiCarWheel />
-      },
-      {
-        title: 'Approve Pending Users',
-        path: '/approve',
-        icon: <GiCartwheel />
-      }
-    ]
-  }
 ];
 
 
